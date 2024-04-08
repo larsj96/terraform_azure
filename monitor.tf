dd4f0e0dea1de12d04bd363e16e525e567a2f0b6
unknown <larsj96@gmail.com>
2024-04-08 16:48:59 +0200

    created both storage policy and a deprecated retention poilicy

--- monitor.tf
+++ monitor.tf
@@ -1,5 +1,10 @@
 
 # make all storage acconts send the logs to the auditing worksapce we created in loganalytics.tf
+
+
+# `retention_policy` has been deprecated in favor of `azurerm_storage_management_policy` resource - to learn more https://aka.ms/diagnostic_settings_log_retention
+# https://learn.microsoft.com/en-us/azure/azure-monitor/essentials/migrate-to-azure-storage-lifecycle-policy?tabs=portal
+
 resource "azurerm_monitor_diagnostic_setting" "storage_account_auditing" {
   for_each            = azurerm_storage_account.storage_account
 
@@ -7,33 +12,20 @@ resource "azurerm_monitor_diagnostic_setting" "storage_account_auditing" {
   target_resource_id  = each.value.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.Auditing-workspace.id
 
-  log {
-    category = "StorageRead"
-    enabled  = true
+ log {
+    category = "AuditEvent"
+    enabled  = false
 
     retention_policy {
-      days    = 30
-      enabled = true
-    }
-  }
-
-  log {
-    category = "StorageWrite"
-    enabled  = true
-
-    retention_policy {
-      days    = 30
-      enabled = true
+      enabled = false
     }
   }
 
   metric {
     category = "AllMetrics"
-    enabled  = true
 
     retention_policy {
-      days    = 30
-      enabled = true
+      enabled = false
     }
   }
 }
--- storageacconts.tf
+++ storageacconts.tf
@@ -1,3 +1,10 @@
+resource "random_string" "storage_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+
 resource "azurerm_storage_account" "storage_account" {
   for_each = azurerm_resource_group.resource_group
 
@@ -10,8 +17,23 @@ resource "azurerm_storage_account" "storage_account" {
   tags = each.value.tags # Gettings tags from the root RG 
 }
 
-resource "random_string" "storage_suffix" {
-  length  = 8
-  special = false
-  upper   = false
+
+
+resource "azurerm_storage_management_policy" "storage_account_policy" {
+  for_each            = azurerm_storage_account.storage_account
+
+  storage_account_id = each.value.id
+
+  rule {
+    name    = "retention-policy"
+    enabled = true
+    filters {
+      blob_types = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 365
+      }
+    }
+  }
 }
