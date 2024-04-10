6cefd8777555af01d680597d5d7dd630a63d4943
unknown <larsj96@gmail.com>
2024-04-10 12:53:05 +0200

    created CNAME DNS records for all the webpages, test,prod and test

--- dns.tf
+++ dns.tf
@@ -1,7 +1,7 @@
 # https://learn.microsoft.com/en-us/azure/dns/dns-get-started-terraform?tabs=azure-cli
 
 
-#azure.lanilsen.xyz -> lanilsen.xyz er styrt av cloudfare
+#azure.lanilsen.xyz -> lanilsen.xyz managed by Terraform on cloudfare
 
 resource "azurerm_dns_zone" "azure_lanilsen_xyz" {
   name                = "azure.lanilsen.xyz"
@@ -9,4 +9,16 @@ resource "azurerm_dns_zone" "azure_lanilsen_xyz" {
 }
 
 
-# SEE OUTPUTS.TF for nameservers
+
+resource "azurerm_dns_cname_record" "web_apps_cname" {
+  for_each = { for key, app in azurerm_static_web_app.static_web_app : key => app.default_host_name }
+
+  name                = each.key  # 'dev', 'prod' and 'test'
+  zone_name           = azurerm_dns_zone.azure_lanilsen_xyz.name
+  resource_group_name = azurerm_resource_group.resource_group["common"].name
+  ttl                 = 3600
+  record              = each.value  # Default hostname for the Azure Static Web App
+}
+
+
+# SEE OUTPUTS.TF for nameservers and this for DNS records outside azure -> https://github.com/larsj96/terraform_cloudfare
