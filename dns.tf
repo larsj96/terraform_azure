d0ae494263aac85d6d09998c0d2c949ad6622988
unknown <larsj96@gmail.com>
2024-04-10 12:06:38 +0200

    Changed from norwayeast to westeurope for the web apps

--- dns.tf
+++ dns.tf
@@ -1,1 +1,9 @@
 # https://learn.microsoft.com/en-us/azure/dns/dns-get-started-terraform?tabs=azure-cli
+
+
+#azure.lanilsen.xyz -> lanilsen.xyz er styrt av cloudfare
+
+resource "azurerm_dns_zone" "azure_lanilsen_xyz" {
+  name                = "azure.lanilsen.xyz"
+  resource_group_name = azurerm_resource_group.resource_group["common"].name
+}
--- staticwebapps.tf
+++ staticwebapps.tf
@@ -1,11 +1,19 @@
 resource "azurerm_static_web_app" "static_web_app" {
   for_each = { for k, v in azurerm_resource_group.resource_group : k => v if k != "common" } # Exclude 'common' resource group
 
-  name                = "Website-mkdocs-${each.key}" # Ensure the name is unique
+  name                = "Website-mkdocs-${each.key}" # Ensures the name is unique
   resource_group_name = azurerm_resource_group.resource_group[each.key].name
-  location            = azurerm_resource_group.resource_group[each.key].location
+  location            = "westeurope" #azurerm_resource_group.resource_group[each.key].location - norwayeast not supported :(
   tags                = azurerm_resource_group.resource_group[each.key].tags
 
   sku_tier   = "Standard"
 
 }
+
+
+
+
+# Error: creating Static Site (Subscription: "87f28a4d-08ed-49ea-a432-a07656c1ac9d" Resource Group Name: "test-resource-group" Static Site Name: "Website-mkdocs-test"): performing CreateOrUpdateStaticSite: unexpected status 400 (400 Bad Request) 
+#with error: LocationNotAvailableForResourceType: The provided location 'norwayeast' is not available for resource type 'Microsoft.Web/staticSites'. List of available regions for the resource type is 'westus2,centralus,eastus2,westeurope,eastasia'.
+# with azurerm_static_web_app.static_web_app["test"]
+# on staticwebapps.tf line 1, in resource "azurerm_static_web_app" "static_web_app":
