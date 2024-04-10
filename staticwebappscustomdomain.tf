91f74a6b20ec8d225108629820d3f4e9b5d437ee
unknown <larsj96@gmail.com>
2024-04-10 13:23:22 +0200

    Added custom domain for all static web apps

--- staticwebappscustomdomain.tf
+++ staticwebappscustomdomain.tf
@@ -1,7 +1,15 @@
 # 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/static_web_app_custom_domain
-# resource "azurerm_static_web_app_custom_domain" "example" {
-#   static_web_app_id = azurerm_static_web_app.example.id
-#   domain_name       = "${azurerm_dns_cname_record.example.name}.${azurerm_dns_cname_record.example.zone_name}"
-#   validation_type   = "cname-delegation"
-# }
+
+
+
+
+# Adding Custom domain to all the websites
+resource "azurerm_static_web_app_custom_domain" "custom_domain" {
+  for_each = azurerm_static_web_app.static_web_app
+
+  static_web_app_id = each.value.id
+  domain_name       = "${azurerm_dns_cname_record.web_apps_cname[each.key].name}.${azurerm_dns_zone.azure_lanilsen_xyz.name}"
+
+  validation_type   = "cname-delegation"
+}
